84377e2a2c5d704d2db08c35433e979fac9e0a86
Laura Salcido <idlaurasalcido@gmail.com>
2018-04-22 09:29:57 -0700

    prompt user

---
+++ routing/apiRoutes.js
@@ -0,0 +1,13 @@
+var friendsData = require("../data/friends")
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData)
+        console.log(friendsData)
+    })
+
+    app.post("/api/friends", function (req, res) {
+        console.log(friendsData)
+    })
+}
+
--- server.js
+++ server.js
@@ -1,29 +1,20 @@
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
 var path = require('path')
 
-var PORT = process.env.PORT || 3000
+var bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + "/app/public/home.html"));
-})
+var PORT = process.env.PORT || 3000
 
-app.get('/survey', function (req, res) {
-    res.sendFile(path.join(__dirname + "/app/public/survey.html"));
-})
-
-var user = 
-app.post('/friends', function(req, res) {
-    var name = req.body.name
-    var photo = req.body.photo
-    res.render('friends', {user : req.body.user})
-})
+var apiRoute = require("./routing/apiRoutes")(app);
+var htmlRoute = require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, function (e) {
     if (e) throw e
+    console.log('listening on port ' + PORT)
 })
+
