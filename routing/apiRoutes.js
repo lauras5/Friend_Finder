7d31acf0a540548e194386dfcdae5c2656b0e6ea
Laura Salcido <idlaurasalcido@gmail.com>
2018-04-26 12:08:47 -0700

    fixing bugs

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,12 +1,44 @@
 var friendsData = require("../data/friends")
 
 module.exports = function (app) {
+    app.post("/api/friends", friendsData, function (req, res) {
+        var userInput = req.body;
+        var userScores = userInput.userScores;
+        var diff = 100;
+        var matchName = '';
+        var matchPhoto = '';
+
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var scoreDiff = 0
+            for (var j = 0; j < userScores.length; j++) {
+                scoreDiff += Math.abs(friendsData[i].scores[j] - userScores[j]);
+            }
+
+            if (scoreDiff < diff) {
+                diff = scoreDiff;
+                matchName = friendsData[i].name;
+                matchPhoto = friendsData[i].photo
+            }
+        }
+        friendsData.push(userInput);
+
+        console.log(friendsData)
+        console.log({
+            matchName: matchName, matchPhoto
+                : matchPhoto
+        })
+        res.json({ matchName: matchName, matchPhoto: matchPhoto })
+    })
+
     app.get("/api/friends", function (req, res) {
         res.json(friendsData)
         // console.log(friendsData)
     })
 
-    app.post("/api/friends", friendsData, function (req, res) {
-        console.log(friendsData)
+    app.get('/userid', function(req, res) {
+        res.send(friendsData.length.toString())
     })
 }
+
+// form validation
