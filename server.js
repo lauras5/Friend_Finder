eada758c7023be6243c20b38dde6d0d0ff38c820
Laura Salcido <idlaurasalcido@gmail.com>
2018-04-26 14:01:25 -0700

    bugs

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var bodyParser = require('body-parser')
 
 var app = express()
 
-var apiRoute = require("./routing/apiRoutes")(app);
-var htmlRoute = require("./routing/htmlRoutes")(app);
-
-// middleware
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 var PORT = process.env.PORT || 3000
 
 app.listen(PORT, function (e) {
     if (e) throw e
     console.log('listening on port ' + PORT)
 })
+
+var apiRoute = require("./routing/apiRoutes");
+var htmlRoute = require("./routing/htmlRoutes");
+
+// middleware
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 
